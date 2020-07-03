087e98238d2776921b84aeddc6bf34b34e4f1744
indrachyd <indraredtrlv@gmail.com>
2020-07-03 19:39:25 +0700

    Initial Commit

---
+++ KitaMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KitaMovies
+//
+//  Created by Indra Cahyadi on 03/07/20.
+//  Copyright © 2020 Indra Cahyadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
